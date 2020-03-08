54a46cb500c535b8f6c44ec62e78961c531cd35e
Jatin Chowdhury <jchowdhu@stanford.edu>
2020-03-08 12:26:16 -0700

    Improved transient detection

--- coder/detect_transients.py
+++ coder/detect_transients.py
@@ -2,34 +2,37 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.io import wavfile
 
-def parTransientDetect(block, thresh=6.4, axis=1):
+def parTransientDetect(block, thresh=16, axis=1):
     """
     Use simple peak-to-avg ratio method to detect transients
     """
-    avg = np.mean(np.abs(block), axis=axis)
-    max = np.max(np.abs(block), axis=axis)
+    mymax = np.max(np.abs(block), axis=axis)
+    argmax = np.argmax(np.abs(block), axis=axis)
+    idxs = np.arange(max(argmax))
+    if len(idxs) == 0:
+        return False
+    avg = np.mean(np.abs(np.take(block, idxs, axis=axis)))
 
-    if any(avg == 0):
+    if np.any(avg == 0):
         return 0
 
-    par = max / avg # peak to avg ratio
-    if any(par > thresh):
+    par = mymax / avg # peak to avg ratio
+    if np.any(par > thresh):
         return True
     return False
 
 if __name__ == "__main__":
-    fs, x = wavfile.read('test_signals/castanet.wav')
-    x = x[25000:225000,:] / 2**15
+    fs, x = wavfile.read('test_signals/glockenspiel.wav')
+    x = x[20000:500000,:] / 2**15
 
     plt.plot(x[:,0])
 
     block_size = 2048
     ts = np.zeros_like(x)
-    thresh = 15
 
     for n in range(0, len(x), block_size):
         block = x[n:n+block_size]
-        ts[n:n+block_size] = float(parTransientDetect(block, axis=0))
+        ts[n:n+block_size] = float(parTransientDetect(block, thresh=16, axis=0))
 
     plt.plot(ts)
 
